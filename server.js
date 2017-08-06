4d2f92ea65f0cbc116bc8886099fd288a41e2e0a
uvaldez <uziel_valdez@hotmail.com>
2017-08-05 22:55:25 -0700

    added server.js

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 
 import Express from 'express';
-import GraphHTTP from 'express-graphql';
-import Schema from './src/backend/schema';
+import path from 'path';
 
 // config
-const APP_PORT = 8000;
+const APP_PORT = (process.env.PORT || 3000);
 const app = new Express();
-app.use('/graphql', GraphHTTP({
-  schema: Schema,
-  pretty: true,
-  graphiql: true,
-}));
+app.use(Express.static(__dirname));
 
+app.get('/', (req,res) => {
+  res.sendFile(path.resolve(__dirname,'src/www/index.html'));
+});
+
+app.get('/bundle.js', (req,res) => {
+  res.sendFile(path.resolve(__dirname,'build/bundle.js'));
+});
 app.listen(APP_PORT, () => {
   console.log(`app listening on port  ${APP_PORT}`);
 });
